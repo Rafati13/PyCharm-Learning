43c6a0841d8741f8ceca4c5f6c0a0426d0caec0d
Mehdi <mehdirafati13@gmail.com>
2023-01-28 16:10:54 +0330

    Add town

--- Code1.py
+++ Code1.py
@@ -2,9 +2,10 @@ def Hi():
     name=input('Please write your name: ')
     lastname=input('Please write your lastname: ')
     age=input('Please write your age: ')
+    town=input('Please enter your town: ')
 
 
-    print(f"Hi {name} {lastname} (age: {age})")
+    print(f"Hi {name} {lastname} (age: {age} from {town})")
 
 Hi()
 
