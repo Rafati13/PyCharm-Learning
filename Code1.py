59ca6bafe24f99d4f00a006cdd63a9ca7b5bdca2
Mehdi <mehdirafati13@gmail.com>
2023-01-28 16:09:28 +0330

    Add age

--- Code1.py
+++ Code1.py
@@ -1,8 +1,10 @@
 def Hi():
     name=input('Please write your name: ')
     lastname=input('Please write your lastname: ')
+    age=input('Please write your age: ')
 
-    print(f"Hi {name} {lastname}")
+
+    print(f"Hi {name} {lastname} (age: {age})")
 
 Hi()
 
